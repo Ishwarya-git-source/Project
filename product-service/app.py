6c336097e6f90d48623b36b300198d206a056867
ishwarya-git-source <ishwaryagupta201@gmail.com>
2025-06-30 14:43:00 +0530

    Test jenkins auto trigger

--- product-service/app.py
+++ product-service/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/products")
 def products():
-    return {"products": ["Laptop", "Phone", "Tablet", "Ishuu"]}
+    return {"products": ["Laptop", "Phone", "Tablet", "Ishwarya"]}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
