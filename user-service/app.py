2742348acefa6cdbae69acfb4ee4bf8979693cf4
ishwarya-git-source <ishwaryagupta201@gmail.com>
2025-07-01 10:26:27 +0530

    automatic testing

--- user-service/app.py
+++ user-service/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/users")
 def users():
-    return {"users": ["Alice", "Bob"]}
+    return {"users": ["Alice", "Bob", "Jhon"]}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
